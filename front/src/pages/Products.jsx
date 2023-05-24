0f67678f6cc54b2a278fa1e144c4d8e7132fe95f
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-05-24 07:53:48 +0200

    Update Products.jsx

--- front/src/pages/Products.jsx
+++ front/src/pages/Products.jsx
@@ -74,6 +74,7 @@ export const Products = () => {
       {adminRole && (
         <AddProductButton onClick={toggleModal}>Add Product</AddProductButton>
       )}
+      <h2>{login}</h2>
       <ProductList products={visibleProducts} />
 
       {showModal && (
